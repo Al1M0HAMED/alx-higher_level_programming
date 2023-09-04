db9fd4aa59fdd1ea68f332f7aac79c8af46ace3f
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-04 16:16:52 +0300

    looked at Floyd's algorithm :"

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,25 +4,19 @@
  * @list: is the head of the linked list.
  * Return: 1 if found an cycle 0 if did not.
  */
-int check_cycle(listint_t *list)
+int check_cycle(listint_t *head)
 {
-	listint_t *current, *tmp;
+	listint_t *sonic = head;
+	listint_t *super_sonic = head;
 
-	if (!list)
+	if (list == NULL)
 		return (0);
-	tmp = list;
-	while (tmp)
-	{	
-		current = list;
-		while (current)
-		{
-			if (current->next == tmp)
-				return (1);
-			current = current->next;
-			if (current == NULL)
-				return (0);
-		}
-		tmp = tmp->next;
+	while (sonic && super_sonic && super_sonic->next)
+	{
+		sonic = sonic->next;
+		super_sonic = super_sonic->next->next;
+		if (super_sonic == sonic)
+			return (1);
 	}
 	return (0);
 }
