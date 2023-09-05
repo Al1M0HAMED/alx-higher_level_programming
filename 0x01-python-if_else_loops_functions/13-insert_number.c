969f9a2ac97e179c103bd125196499396c4f3aba
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-05 19:16:47 +0300

    Solving a problem is already a big win! but finding the best and optimal way to solve it, it’s way better! Think about the most optimal / fastest way to do it.

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,31 +10,27 @@ listint_t *insert_node(listint_t **head, int number)
 	int i = 0, n[50];
 	listint_t *new_node, *h = *head;
 
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = number;
+
 	if (*head == NULL || number < (*head)->n)
 	{
-		new_node = malloc(sizeof(listint_t));
-		if (!new_node)
-			return (NULL);
-		new_node->n = number;
-		if (*head == NULL)
-			new_node->next = NULL;
-		else
-			new_node->next = *head;
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
+
 	for (; h->next != NULL; i++)
 		h = h->next,  n[i] = h->n;
 	for (i = i - 1, h = *head; n[i] > number;)
 		i--;
+
 	while (h != NULL)
 	{
 		if (h->n == n[i])
 		{
-			new_node = malloc(sizeof(listint_t));
-			if (!new_node)
-				return (NULL);
-			new_node->n = number;
 			new_node->next = h->next;
 			h->next = new_node;
 			return (new_node);
