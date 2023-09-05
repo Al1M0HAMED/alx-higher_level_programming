340427452c0c508d4483a3e2ccd5ff5e6ef19363
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-05 15:48:33 +0300

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# this function prints a string in uppercase followed by a new line.
-
 def uppercase(str):
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
