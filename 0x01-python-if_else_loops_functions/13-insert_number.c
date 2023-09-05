78270a5411b9a6df8ddc488f1507b794b18e97f2
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-05 18:20:06 +0300

    Solving a problem is already a big win! but finding the best and optimal way to solve it, it’s way better! Think about the most optimal / fastest way to do it.

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,30 +7,26 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	int i = 0;
-	int n[50];
+	int i = 0, n[50];
 	listint_t *new_node, *h = *head;
 
-	if (h == NULL)
+	if (*head == NULL || number < (*head)->n)
 	{
 		new_node = malloc(sizeof(listint_t));
 		if (!new_node)
 			return (NULL);
 		new_node->n = number;
-		new_node->next = NULL;
-		h = new_node;
-		return (new_node);
+		if (*head == NULL)
+			new_node->next = NULL;
+		else
+			new_node->next = (*head)->next;
+		*head = new_node;
+		return (*head);
 	}
-	while(h->next != NULL)
-	{
-		h = h->next;
-		n[i] = h->n;
-		i++;
-	}
-	i -= 1;
-	while(n[i] > number)
+	for (; h->next != NULL; i++)
+		h = h->next,  n[i] = h->n;
+	for (i = i - 1, h = *head; n[i] > number;)
 		i--;
-	h = *head;
 	while (h != NULL)
 	{
 		if (h->n == n[i])
