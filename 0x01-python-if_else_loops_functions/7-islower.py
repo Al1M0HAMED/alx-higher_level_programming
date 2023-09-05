84d9791116bd0aac1a8f12543560974642b7ee9f
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-05 14:18:41 +0300

    odd(man odd)

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c != '\0':
-        if c >= 'a' and c <= 'z':
-            return True
-        else:
-            return False
+    if ord(c) >= 'a' and ord(c) <= 'z':
+        return True
+    else:
+        return False
