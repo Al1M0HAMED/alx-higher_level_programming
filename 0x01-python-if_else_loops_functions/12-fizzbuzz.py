48b43d52c62abddaca0b39313817df2f7c6659f3
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-05 16:30:18 +0300

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,12 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-            if (i % 3 == 0 and i % 5 == 0):
-                status = "FizzBuzz"
-            elif (i % 5 == 0):
-                status = "Buzz"
-            elif (i % 3 == 0):
-                status = "Fizz"
-            else:
-                status = i
-            print(status, end=" ")
+        if (i % 3 == 0 and i % 5 == 0):
+            status = "FizzBuzz"
+        elif (i % 5 == 0):
+            status = "Buzz"
+        elif (i % 3 == 0):
+            status = "Fizz"
+        else:
+            status = i
+        print(status, end=" ")
