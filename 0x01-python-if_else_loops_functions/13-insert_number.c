1ef58f77b5bf4932e0b388138ebc11ec6a0fe7ab
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-05 16:56:59 +0300

    interveiw problem

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * insert_node - this function inserts a node to a linked list.
+ * @number: is the new node n value
+ * @head: is the linked list.
+ * Return: adress of new node or NULL if failed.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node, *h = *head;
+
+	while (h != NULL)
+	{
+		if (h->next->n > number)
+		{
+			new_node = malloc(sizeof(listint_t));
+			if (!new_node)
+				return (NULL);
+			new_node->n = number;
+			new_node->next = h->next;
+			h->next = new_node;
+			return (new_node);
+		}
+		h = h->next;
+	}
+	return (NULL);
+}
