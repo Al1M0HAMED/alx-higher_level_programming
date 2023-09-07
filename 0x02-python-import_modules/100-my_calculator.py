75429aebcf3be5fefa925a641289451e586442b5
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-07 18:39:09 +0300

    advanced scripts

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,10 +3,22 @@ import sys
 from locale import atoi
 from calculator_1 import sub, mul, add, div
 if __name__ == '__main__':
-    av = sys.av
+    av = sys.argv
     ac = len(av)
     if ac != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if av[2] != '+' or '-' or '*' or '/':
-
+    op = av[2]
+    a, b = atoi(av[1]), atoi(av[3])
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif op == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    sys.exit()
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == '__main__':
     arguments = sys.argv
     argc = len(arguments)
-    print("{} arguments.".format(argc - 1))
+    print("{} argument{}".format(argc - 1, "s." if argc == 1 else ":"))
     if argc > 1:
         for i in range(1, argc):
             print("{}: {}".format(i, arguments[i]))
