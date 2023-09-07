6dd73df7b88bfc7e73cec5fa574f8c5cdb2bd70d
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-07 14:55:41 +0300

    what is the requarement

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as add
+import add_0 as s
 if __name__ == '__main__':
     a = 1 
     b = 2
-    print("{} + {} = {}".format(a, b, add.add(a, b)))
+    print("{} + {} = {}".format(a, b, s.add(a, b)))
