e2932facad5ed6b0f709074726c233d8a18310e7
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-07 19:07:34 +0300

    last touch

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,11 @@ import sys
 if __name__ == '__main__':
     arguments = sys.argv
     ac = len(arguments)
-    print("{} argument{}{}".format(ac - 1, "s" if ac != 2 else "", ":" if ac > 1 else "."))
+    print("{} argument{}{}".format(
+        ac - 1,
+        "s" if ac != 2 else "",
+        ":" if ac > 1 else "."
+    ))
     if ac > 1:
         for i in range(1, ac):
             print("{}: {}".format(i, arguments[i]))
