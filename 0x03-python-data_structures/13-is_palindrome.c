fad64fe411e457bba31d0b98492693d66ee66c92
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-12 17:18:00 +0300

    the most fast solution i was able to get

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,47 +7,67 @@
  */
 int is_palindrome(listint_t **head)
 {
-	size_t len;
-	int idx, arr[2056], i;
-	listint_t *current = *head;
+	listint_t *mid = *head, *first = *head, *last;
+	size_t idx, len;
 
 	if (!*head || !(*head)->next)
 		return (1);
 
 	len = listint_len(*head);
-	idx = (len / 2), i = 0;
-	while (idx && current)
+	if (len % 2)
+		idx = (len / 2) + 1;
+	else
+		idx = (len / 2);
+
+	while (idx > 0)
+		mid = mid->next, idx--;
+
+	last = reverse_listint(&mid);
+	mid = last;
+
+	while (first && last)
 	{
-		arr[i] = current->n;
-		current = current->next;
-		idx--, i++;
-	}
-	if (len % 2 != 0)
-		current = current->next;
-	idx = 0, i--;
-	while (i >= 0)
-	{
-		if ((current == NULL && i) || arr[i] != current->n)
+		if (last->n != first->n)
+		{
+			reverse_listint(&mid);
 			return (0);
-		current = current->next, i--;
+		}
+		first = first->next;
+		last = last->next;
 	}
+	reverse_listint(&mid);
 	return (1);
 }
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of nodes
+ * reverse_listint - this function reverse a linked list.
+ * @head: is the linked list address.
+ * Return: the address of first node of the reversed linked list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next, *previos;
+
+	next = NULL, previos = NULL;
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = previos;
+		previos = *head;
+		*head = next;
+	}
+	*head = previos;
+	return (*head);
+}
+/**
+ * listint_len - this function returns the number of elements in a linked.
+ * @h: is the address of a linked list.
+ * Return: the len of a linked list.
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	size_t len = 0;
 
-	while (h)
-	{
-		num++;
-		h = h->next;
-	}
-
-	return (num);
+	while (h != NULL)
+		len++, h = h->next;
+	return (len);
 }
