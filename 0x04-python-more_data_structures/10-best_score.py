56bc9f56ac347dde12811196c5acda8c6ba384c4
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-14 04:14:11 +0300

    biggest_element bug

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return (None)
-    i = 0
-    biggest_element, biggest_key = int(), str()
-    for key, element in a_dictionary.items():
-        if i == 0 or element >= biggest_element:
-            biggest_key = key
-            biggest_element = element
-        i += 1
-    return (biggest_key)
+    return (max(a_dictionary, key=a_dictionary.get))
