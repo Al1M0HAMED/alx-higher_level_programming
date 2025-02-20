eec9f6f0d88da0c758c6db6e70b83ee56a8ecff6
Ali Mohamed <mohamed37222721@gmail.com>
2025-02-21 01:14:47 +0200

    aha

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,16 +5,17 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
+    j = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
         except (TypeError, ValueError):
+            j += 1
             pass
-            i -= 1
         except:
             raise
         else:
             i += 1
         
     print("")
-    return (i)
+    return (i - j)
