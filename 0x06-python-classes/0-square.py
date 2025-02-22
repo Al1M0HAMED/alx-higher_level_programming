0b864cd6537e3b2f58cffc1dbbbba3545ac1eb0f
Al1m0hamed <mohamed37222721@gmail.com>
2025-02-22 08:33:32 +0200

    documinting

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,7 @@
 
 
 class Square:
+    """A simple square class that performs basic operations."""
     def __init__(self):
+        """creates a square"""
         pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,5 +2,7 @@
 
 
 class Square:
+    """A simple class that performs basic operations."""
     def __init__(self, size):
+        """creates a square"""
         self.__size = size
