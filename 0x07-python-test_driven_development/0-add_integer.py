c67b26a0b7b0f8629e54975b9a3d93e41805b14f
Al1m0hamed <mohamed37222721@gmail.com>
2025-02-24 06:00:01 +0200

    not so awesome

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,10 +7,9 @@ uses Tests
 def add_integer(a, b=98):
     """ a function that adds 2 integers. """
 
-    if a is None or (not isinstance(a, int) and not isinstance(a, float)):
+    if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
-
-    if not isinstance(b, int) and not isinstance(b, float):
+    if (not isinstance(b, int) and not isinstance(b, float)):
         raise TypeError("b must be an integer")
 
     return (int(a) + int(b))
