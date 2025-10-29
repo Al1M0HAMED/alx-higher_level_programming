4919b114787c9e764b488b76c0557a8ab3cd1628
Al1M0HAMED <mohamed37222721@gmail.com>
2025-10-29 15:56:49 +0300

    I'm Back

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module performs oop learning operations.
+
+Author: Ali Mohamed
+Date: 2025-10-29
+"""
+
+
+class Rectangle:
+    """A simple square class that performs basic operations."""
+    def __init__(self):
+        """ creates a square """
+        pass
