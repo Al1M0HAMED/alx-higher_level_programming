0dc07ff12ea9d4c30687b8b796a782b53cfa8fa1
Al1M0HAMED <mohamed37222721@gmail.com>
2025-11-06 17:06:57 +0200

    del

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""
+This module performs oop learning operations.
+
+Author: Ali Mohamed
+Date: 2025-11-1
+"""
+
+
+class Rectangle:
+    """A simple square class that performs basic operations."""
+    def __init__(self, width=0, height=0):
+        """ creates a square """
+        self.__width = width
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        else:
+            if width < 0:
+                raise ValueError("width must be >= 0")
+        self.__height = height
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        else:
+            if height < 0:
+                raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        """ retrieve width """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """" set width value """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ retrieve height """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """" set height value """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """ retrieve area """
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        """ retrieve perimeter """
+        if self.__height == 0 or self.__width == 0:
+            return (0)
+        return ((self.__height + self.__width) * 2)
+
+    def __str__(self):
+        parts = []
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        else:
+            for i in range(self.__height):
+                if i > 0:
+                    parts.append("\n")
+                for j in range(self.__width):
+                    parts.append("#")
+            rectangle_str = ''.join(parts)
+            return (rectangle_str)
+
+    def __repr__(self):
+        return (f"Rectangle({self.__width}, {self.__height})")
+
+    def __del__(self):
+        print("Bye rectangle...")
