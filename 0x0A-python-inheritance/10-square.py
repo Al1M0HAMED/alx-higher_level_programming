13322dd15ea566ea6cb804267f40996b549f3f65
Al1M0HAMED <mohamed37222721@gmail.com>
2025-11-08 18:44:06 +0200

    road to victory

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+date: 8 / 11 / 2025
+auther: Ali Mohamed
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ basic square operation that inherits from rectangle """
+
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", self.__size)
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
