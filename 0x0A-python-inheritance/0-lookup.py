5ff2aa3285181acd853e75f1113d3ede3095bd9c
Al1M0HAMED <mohamed37222721@gmail.com>
2025-11-08 18:59:20 +0200

    lesgoo

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+
+Date: 8/11/2025
+Auther: Ali Mohamed
+
+"""
+
+
+def lookup(obj):
+    """ returns the list of available attributes and methods of an object """
+    return (list(dir(obj)))
