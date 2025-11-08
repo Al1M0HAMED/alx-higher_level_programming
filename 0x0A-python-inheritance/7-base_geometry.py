36928018ef4c2d6e49b1054a8be65f6de0cb2997
Al1M0HAMED <mohamed37222721@gmail.com>
2025-11-08 07:07:50 +0200

    road to victory

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-""" 
-This module performs oop learning operations.
-
-Author: Ali Mohamed
-Date: 2025-11-8
-
+"""
+Contains the class BaseGeometry
 """
 
 
 class BaseGeometry:
-    """ simple inheritance operations """
-
+    """A class with public instance methods area and integer_validator"""
     def area(self):
+        """raises an exception when called"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
