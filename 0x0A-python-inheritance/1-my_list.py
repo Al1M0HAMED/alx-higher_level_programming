90e143b7a97858e8babaf3256923bbfdf76f4359
Al1M0HAMED <mohamed37222721@gmail.com>
2025-11-15 19:09:22 +0200

    burn

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,8 @@ auther: Ali Mohamed
 
 class MyList(list):
     """" basic list operations """
+    def __init__(self):
+        super().__init__()
+
     def print_sorted(self):
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
