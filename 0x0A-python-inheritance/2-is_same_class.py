61c8281f97f04dda4fe66cf232a41a8b649212f8
Al1M0HAMED <mohamed37222721@gmail.com>
2025-11-15 19:30:12 +0200

    burn!

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,5 +3,9 @@
 date: 15/ 11 / 2025
 auther: Ali Mohamed
 """
+
+
 def is_same_class(obj, a_class):
+    """returns True if the object is exactly an instance of the specified class
+    otherwise False. """
     return type(obj) is a_class
