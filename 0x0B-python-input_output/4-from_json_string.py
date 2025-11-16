b0ea600e7526190ba162c74ff1e47b030abd01d1
Al1M0HAMED <mohamed37222721@gmail.com>
2025-11-16 03:36:43 +0200

    fuigoo

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Auther: Ali Mohamed
+Date: 16/11/2025
+"""
+import json
+
+
+def from_json_string(my_str):
+    """returns the JSON representation of an object (string)."""
+    return json.loads(my_str)
