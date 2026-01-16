2e7e3ec272cbde67b4102cda12debbbc8bdb7ce2
Al1M0HAMED <mohamed37222721@gmail.com>
2026-01-16 20:47:46 +0200

    bioooo

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+"""
+7-add_item.py
+
+Module to add all command line arguments to a JSON file.
+
+This script does the following:
+1. Ensures the JSON file exists (creates it if it doesn't).
+2. Loads the current list from the JSON file if it contains data.
+3. Adds all command line arguments (excluding the script name) to the list.
+4. Saves the updated list back to the JSON file.
+
+Usage:
+    ./7-add_item.py <item1> <item2> ... <itemN>
+
+Modules Imported:
+- sys: To access command line arguments.
+- os: To check file size and existence.
+- 6-load_from_json_file: Custom module to load a list from a JSON file.
+- 5-save_to_json_file: Custom module to save a list to a JSON file.
+"""
 
 import sys
 import os
