d5b7fe680fdfee9fe79c0d7a6d88ce913e8123ca
Al1M0HAMED <mohamed37222721@gmail.com>
2026-01-17 06:50:23 +0200

    pascal is a great guy

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+pascal p
+"""
+
+
+def pascal_triangle(n):
+    pascal_list = []
+    i = 0
+    while i < n:
+        inner_list = []
+        j = 0
+        while j <= i:
+            if j == i or i == 0 or i == 1 or j == 0:
+                inner_list.append(1)
+            else:
+                inner_list.append(pascal_list[i - 1][j]
+                                  + pascal_list[i - 1][j - 1])
+            j += 1
+        pascal_list.append(inner_list)
+        i += 1
+    return (pascal_list)
