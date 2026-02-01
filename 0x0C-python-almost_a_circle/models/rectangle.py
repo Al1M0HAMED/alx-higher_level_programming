dcdb1d7048a84c9aa8005e687834d7c5129d78ac
Ali Mohamed <alimohamed@192.168.1.34>
2026-02-01 06:01:19 +0200

    pycodestyle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -51,7 +51,8 @@ class Rectangle(Base):
         """
         str
         """
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                self.__x, self.__y, self.__width, self.__height))
 
     @property
     def width(self):
