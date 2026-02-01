3146b1026ec212977e832dfa07cff4c499bd8f07
Ali Mohamed <alimohamed@192.168.1.34>
2026-02-01 05:00:57 +0200

    c+MA=PLi'b

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,4 +82,4 @@ class Rectangle(Base):
         elif valid == 1:
             raise TypeError("y must be an integer")
         else:
-            raise ValueError("y must be >=> 0")
+            raise ValueError("y must be >= 0")
