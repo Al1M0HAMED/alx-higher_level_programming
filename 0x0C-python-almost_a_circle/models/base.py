84417dbbb0dfecf9cc1e9ae8fa85e134fa1a06cf
Ali Mohamed <alimohamed@192.168.1.15>
2026-02-02 07:01:36 +0200

    to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,3 +29,16 @@ class Base:
             return (json.dumps(list_dictionaries))
         else:
             return ("[]")
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        save to a file
+        """
+        filename = f"{cls.__name__}.json"
+        if list_objs is not None and len(list_objs) >= 1:
+            list_dicts = [obj.to_dictionary() for obj in list_objs]
+        else:
+            list_dicts = []
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_dicts))
