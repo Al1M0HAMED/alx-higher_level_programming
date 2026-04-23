e758e2bcab5915e523ce5865911c4bc7220e8742
Ali Mohamed <alimohamed@192.168.1.11>
2026-04-23 04:07:41 +0200

    unittest is fun

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,7 +10,11 @@ class TestMaxInteger(unittest.TestCase):
     def test_pass(self):
         self.assertEqual(max_integer([0, 5, 3, 2, 2]), 5)
         self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([-1]), -1)
+        self.assertEqual(max_integer([1, 2, 5, 4, 3]), 5)
+        self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
         self.assertEqual(max_integer([5]), 5)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
     def test_fail(self):
         self.assertRaises(TypeError, max_integer, 1.5)
