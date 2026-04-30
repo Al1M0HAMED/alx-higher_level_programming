43e5c2c4eda691283b4d2a41ced25f93318cd871
Ali Mohamed <alimohamed@192.168.1.3>
2026-04-30 03:44:43 +0300

    create and gave all priv

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,8 @@
--- cteare user and gave all grants.
-CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO 'username'@'%';
+-- create user and grant privileges
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
 FLUSH PRIVILEGES;
