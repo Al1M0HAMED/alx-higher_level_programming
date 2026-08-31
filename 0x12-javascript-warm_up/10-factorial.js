13162624eef306548afb6704e393c7cfbe95b8f0
Ali Mohamed <alimohamed@Alis-MacBook-Air.local>
2026-08-31 12:40:42 +0300

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (n === 1) {
+    return (1);
+  } else if (n > 1) {
+    return (n + factorial(n - 1));
+  } else {
+    return (1);
+  }
+}
+const n = parseInt(process.argv[2], 10);
+if (Number.isNaN(n)) {
+  console.log(1);
+} else {
+  console.log(factorial(n));
+}
