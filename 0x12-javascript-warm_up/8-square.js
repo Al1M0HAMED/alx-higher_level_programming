ca8d2bbbccea784db13d9937ed74737e8bc4c0d9
Ali Mohamed <alimohamed@Alis-MacBook-Air.local>
2026-08-31 11:38:45 +0300

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const x = parseInt(process.argv[2], 10);
+
+if (!(Number.isNaN(x))) {
+  let i = x;
+  while (i > 0) {
+    let j = x;
+    while (j) {
+      process.stdout.write('*');
+      j--;
+    }
+    console.log('');
+    i--;
+  }
+} else {
+  console.log('Missing size');
+}
