6baa296b8cbb3fc662ad3e597128b0d7e3e08a3f
Ali Mohamed <alimohamed@Alis-MacBook-Air.local>
2026-08-31 13:44:23 +0300

    wow

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -9,8 +9,8 @@ if (!(len <= 3)) {
 
   while (i > 1) {
     const n = parseInt(process.argv[i], 10);
-    if (n > biggest || Number.isNaN(biggest)) { biggest = n; }
-    if ((n > secBig && n < biggest) || (Number.isNaN(secBig) && n != biggest)) { secBig = n; }
+	if (Number.isNaN(secBig)) { secBig = n; }
+	if (n > biggest || Number.isNaN(biggest)) {secBig = biggest; biggest = n; }
     i--;
   }
   console.log(secBig);
