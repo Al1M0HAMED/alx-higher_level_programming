969b9702d7ca6a1370449d0905b0e056bccd4140
Ali Mohamed <alimohamed@Alis-MacBook-Air.local>
2026-08-31 13:08:53 +0300

    lesgoo

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const len = process.argv.length;
+let biggest = NaN;
+let secBig = NaN;
+
+if (!(len <= 3)) {
+  let i = len - 1;
+
+  while (i > 1) {
+    const n = parseInt(process.argv[i], 10);
+    if (n > biggest || Number.isNaN(biggest)) { biggest = n; }
+    if ((n > secBig && n < biggest) || Number.isNaN(secBig)) { secBig = n; }
+    i--;
+  }
+  console.log(secBig);
+} else {
+  console.log(0);
+}
