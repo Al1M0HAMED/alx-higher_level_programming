eae28a4711949756acf65264928b92f8ff36d307
Ali Mohamed <alimohamed@Alis-MacBook-Air.local>
2026-08-31 11:51:35 +0300

    k

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function add(a, b) {
+  const first = parseInt(a, 10);
+  const seccond = parseInt(b, 10);
+
+  if (!(Number.isNaN(first) || (Number.isNaN(seccond)))) {
+    const sum = (first + seccond);
+    console.log(sum);
+  } else {
+    console.log('NaN');
+  }
+}
+add(process.argv[2], process.argv[3]);
