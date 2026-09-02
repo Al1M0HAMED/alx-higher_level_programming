60ed09edf095c9163af88a1b463e31abc079b3f5
Ali Mohamed <alimohamed@Alis-MacBook-Air.local>
2026-09-02 17:35:42 +0300

    reverse list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,15 @@
+#!/sr/bin/node
+
+exports.esrever = function (list) {
+  let i = list.length;
+  let j = 0;
+
+  while (i > j) {
+    i--;
+    const temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+    j++;
+  }
+  return (list);
+};
